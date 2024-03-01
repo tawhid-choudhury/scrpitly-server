1a1321d131358967adee424d46a82e607e741489
pktatari <tawhidalmuhaimin@gmail.com>
2024-03-01 11:33:59 +0600

    added featured endpoint

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "https://article-publishing-website-client.vercel.app",
+      "https://scriptlys.vercel.app",
     ],
     credentials: true,
   })
